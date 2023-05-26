303162d28d5581f72e503bbd13a4ad5f0874d1e3
Jerry Buck <jerry.buck@pt.lu>
2023-05-26 12:11:29 +0200

    Modified seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,5 @@ movies = JSON.parse(movies_serialized)
 movies["results"].each do |movie|
   Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w600_and_h900_bestv2#{movie["poster_path"]}", rating: movie["vote_average"])
 end
+
+List.create(name: "Comedy")
